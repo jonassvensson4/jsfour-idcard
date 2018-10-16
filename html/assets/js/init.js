9465e0d11cca9287380b18b8d48e619268508209
Jonas Svensson <jonassvensson4@hotmail.com>
2018-10-16 21:35:24 +0200

    Update init.js

--- html/assets/js/init.js
+++ html/assets/js/init.js
@@ -6,23 +6,7 @@ $(document).ready(function(){
       var userData    = event.data.array['user'][0];
       var licenseData = event.data.array['licenses'];
       var sex         = userData.sex;
-
-      if ( licenseData != null ) {
-        Object.keys(licenseData).forEach(function(key) {
-          var type = licenseData[key].type;
-
-          if ( type == 'drive_bike') {
-            type = 'bike';
-          } else if ( type == 'drive_truck' ) {
-            type = 'truck';
-          } else if ( type == 'drive' ) {
-            type = 'car';
-          }
-
-          $('#licenses').append('<p>'+ type +'</p>');
-        });
-      }
-
+      
       if ( sex.toLowerCase() == 'm' ) {
         $('img').attr('src', 'assets/images/male.png');
         $('#sex').text('male');
@@ -37,6 +21,22 @@ $(document).ready(function(){
       $('#signature').text(userData.firstname + ' ' + userData.lastname);
 
       if ( type == 'driver' ) {
+        if ( licenseData != null ) {
+        Object.keys(licenseData).forEach(function(key) {
+          var type = licenseData[key].type;
+
+          if ( type == 'drive_bike') {
+            type = 'bike';
+          } else if ( type == 'drive_truck' ) {
+            type = 'truck';
+          } else if ( type == 'drive' ) {
+            type = 'car';
+          }
+
+          $('#licenses').append('<p>'+ type +'</p>');
+        });
+      }
+        
         $('#id-card').css('background', 'url(assets/images/license.png)');
       } else {
         $('#id-card').css('background', 'url(assets/images/idcard.png)');
