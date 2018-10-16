58ec461ecc7e8a2727ef1acadd1176d870baf61e
Jonas Svensson <jonassvensson4@hotmail.com>
2018-10-16 18:33:24 +0200

    Licenses added

--- server.lua
+++ server.lua
@@ -9,9 +9,16 @@ AddEventHandler('jsfour-idcard:open', function(ID, targetID, type)
 	local _source 	 = ESX.GetPlayerFromId(targetID).source
 
 	MySQL.Async.fetchAll('SELECT firstname, lastname, dateofbirth, sex, height FROM users WHERE identifier = @identifier', {['@identifier'] = identifier},
-	function (result)
-		if (result[1] ~= nil) then
-		  TriggerClientEvent('jsfour-idcard:open', _source, result, type)
+	function (user)
+		if (user[1] ~= nil) then
+			MySQL.Async.fetchAll('SELECT type FROM user_licenses WHERE owner = @identifier', {['@identifier'] = identifier},
+			function (licenses)
+				local array = {
+					user = user,
+					licenses = licenses
+				}
+				TriggerClientEvent('jsfour-idcard:open', _source, array, type)
+			end)
 		end
 	end)
 end)
