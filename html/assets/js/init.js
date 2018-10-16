f1567ebd3a98d7fdb6a2a8d02cdb53ad24b6c0b5
Jonas Svensson <jonassvensson4@hotmail.com>
2018-10-16 18:32:20 +0200

    Licenses added

--- html/assets/js/init.js
+++ html/assets/js/init.js
@@ -2,9 +2,26 @@ $(document).ready(function(){
   // LUA listener
   window.addEventListener('message', function( event ) {
     if (event.data.action == 'open') {
-      var type = event.data.type;
-      var data = event.data.array[0];
-      var sex  = data.sex;
+      var type        = event.data.type;
+      var userData    = event.data.array['user'][0];
+      var licenseData = event.data.array['licenses'];
+      var sex         = userData.sex;
+
+      if ( licenseData != null ) {
+        Object.keys(licenseData).forEach(function(key) {
+          var type = licenseData[key].type;
+
+          if ( type == 'drive_bike') {
+            type = 'bike';
+          } else if ( type == 'drive_truck' ) {
+            type = 'truck';
+          } else if ( type == 'drive' ) {
+            type = 'car';
+          }
+
+          $('#licenses').append('<p>'+ type +'</p>');
+        });
+      }
 
       if ( sex.toLowerCase() == 'm' ) {
         $('img').attr('src', 'assets/images/male.png');
@@ -14,10 +31,10 @@ $(document).ready(function(){
         $('#sex').text('female');
       }
 
-      $('#name').text(data.firstname + ' ' + data.lastname);
-      $('#dob').text(data.dateofbirth);
-      $('#height').text(data.height);
-      $('#signature').text(data.firstname + ' ' + data.lastname);
+      $('#name').text(userData.firstname + ' ' + userData.lastname);
+      $('#dob').text(userData.dateofbirth);
+      $('#height').text(userData.height);
+      $('#signature').text(userData.firstname + ' ' + userData.lastname);
 
       if ( type == 'driver' ) {
         $('#id-card').css('background', 'url(assets/images/driver.png)');
