c543cc0f684f39dae5d2d02f1a2d3df825d752b7
Jonas Svensson <jonassvensson4@hotmail.com>
2019-04-17 10:03:34 +0200

    License check

--- server.lua
+++ server.lua
@@ -7,17 +7,38 @@ RegisterServerEvent('jsfour-idcard:open')
 AddEventHandler('jsfour-idcard:open', function(ID, targetID, type)
 	local identifier = ESX.GetPlayerFromId(ID).identifier
 	local _source 	 = ESX.GetPlayerFromId(targetID).source
+	local show       = false
 
 	MySQL.Async.fetchAll('SELECT firstname, lastname, dateofbirth, sex, height FROM users WHERE identifier = @identifier', {['@identifier'] = identifier},
 	function (user)
 		if (user[1] ~= nil) then
 			MySQL.Async.fetchAll('SELECT type FROM user_licenses WHERE owner = @identifier', {['@identifier'] = identifier},
 			function (licenses)
-				local array = {
-					user = user,
-					licenses = licenses
-				}
-				TriggerClientEvent('jsfour-idcard:open', _source, array, type)
+				if type ~= nil then
+					for i=1, #licenses, 1 do
+						if type == 'driver' then
+							if licenses[i].type == 'drive' or licenses[i].type == 'drive_bike' or licenses[i].type == 'drive_truck' then
+								show = true
+							end
+						elseif type =='weapon' then
+							if licenses[i].type == 'weapon' then
+								show = true
+							end
+						end
+					end
+				else
+					show = true
+				end
+
+				if show then
+					local array = {
+						user = user,
+						licenses = licenses
+					}
+					TriggerClientEvent('jsfour-idcard:open', _source, array, type)
+				else
+					TriggerClientEvent('esx:showNotification', _source, "You don't have that type of license..")
+				end
 			end)
 		end
 	end)
